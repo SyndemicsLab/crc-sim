a55bf87e49be3bbc97cb7c50025bd9c0cfd81c35
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2025-07-29 12:00:56 -0400

    add multi-dataset emulation to plot and data generators

--- Scripts/plotGenerator.R
+++ Scripts/plotGenerator.R
@@ -67,6 +67,28 @@ ggplot(pct[!Model %like% "Both"], aes(x = Group, y = pct, group = Group)) +
         axis.title = element_text(size = 22),
         strip.text = element_text(size = 15))
 
-ggsave("test.png", width = 8, height = 8)
+# Many data emulation
+final <- fread("../Data/PHDEmulation.csv")
+
+ground_truth <- setorderv(ground_truth, "N_ID", order = 1L)
+plot_data <- copy(final)[, pct_diff := (estimates - gt) / gt * 100
+                         ][, model := ifelse(model == "poisson", "Poisson", "NB")
+                           ][, params := paste0(stringr::str_to_title(direction), "-", threshold)
+                             ][, group := factor(group, levels = ground_truth$strata, labels = ground_truth$N_ID)]
+library(ggplot2)
+ggplot(plot_data[!direction %like% "both"], aes(x = group, y = pct_diff, group = group)) + 
+  geom_hline(yintercept = 0, col = "red", linetype = "dashed") +
+  geom_boxplot() + 
+  facet_grid(params~model) + 
+  labs(x = "Ground Truth Population",
+       y = "Percent Difference in\nEstimate from Ground Truth",
+       title = "Single Dataset Bootstrapping",
+       subtitle = "Boostrapped Suppression Values ") +
+  theme_bw() +
+  theme(axis.text = element_text(size = 15),
+        axis.title = element_text(size = 22),
+        strip.text = element_text(size = 15))
+ggsave("../Figures/PHDEmulation.png", width = 8, height = 8)
+
 
 
