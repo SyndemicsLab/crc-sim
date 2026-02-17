c0643fb48451bdeecaaed8d8987182c34e851d1e
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2026-02-17 11:53:51 -0500

    stupid nesting for CRAN to play nice with GitHub

---
+++ crc-sim/R/simulate.R
@@ -0,0 +1,65 @@
+#' Attempts recovery of the ground truth - fundamentally a wrapper for Syndemics::crc
+#'
+#' @importFrom Syndemics crc
+#' @param DT data.table from the \code{create.data} step
+#' @param capture list: strings of captures
+#' @param group stratification string to group by
+#' @param suppress numeric: maximum value to suppress at
+#' @param method string: selection for spatial capture recapture model - either "poisson" or "negbin"
+#' @param formula.selection string: selection for formula decision - either "aic", "corr", or "stepwise"
+#' @param opts.stepwise list: list containing 'direction' of 'forward' 'backward' or 'both', and 'threshold': p value threshold for stepwise selection
+#'
+#' @importFrom Syndemics crc
+#' @keywords internal
+simulate <- function(
+    DT,
+    capture = c("APCD", "BSAS", "Casemix", "Death", "Matris", "PMP"),
+    group,
+    suppress,
+    method,
+    formula.selection,
+    opts.stepwise
+) {
+    N_ID <- NULL
+    tmp <- NULL
+
+    DT <- DT[, tmp := rowSums(.SD), .SDcols = capture][tmp != 0, ][,
+        tmp := NULL
+    ]
+
+    if (suppress) {
+        DT <- DT[!N_ID %in% 1:suppress, ]
+    }
+
+    if (!missing(group)) {
+        DT.list <- c()
+        n_groups <- sort(unique(DT[[group]]))
+
+        for (i in seq_along(n_groups)) {
+            DT.list[[i]] <- DT[get(group) == n_groups[i], ][,
+                paste0(group) := NULL
+            ]
+        }
+        out.list <- lapply(DT.list, function(x) {
+            Syndemics::crc(
+                x,
+                "N_ID",
+                binary.variables = capture,
+                method,
+                formula.selection,
+                opts.stepwise = opts.stepwise
+            )
+        })
+        return(out.list)
+    } else {
+        out <- Syndemics::crc(
+            DT,
+            "N_ID",
+            binary.variables = capture,
+            method,
+            formula.selection,
+            opts.stepwise = opts.stepwise
+        )
+        return(out)
+    }
+}
--- crc-sim/README.md
+++ crc-sim/README.md
@@ -8,7 +8,7 @@ This repository is under development and being converted to an analysis reposito
 
 ## Overview
 
-<img align="right" src="crc-sim/data/crc-sim.png" style="float" width="200">
+<img align="right" src="data/crc-sim.png" style="float" width="200">
 
 To effectively tackle the overdose crisis, a nuanced understanding of Opioid Use Disorder (OUD) prevalence is crucial, both broadly and within targeted cohorts. Healthcare interactions provide estimates but may overlook those outside the healthcare system, leading to underestimation. Capture-recapture (CRC) analysis is valuable in estimating prevalence by addressing underreporting in surveillance. Conventionally, a stepwise model selection process (MSP) is employed to identify the model that best fits the data. However, the MSP in estimating group-stratified prevalence is less explored, especially with sparse data. This study uses simulations to investigate different MSPs for selecting conventional log-linear CRC models, with a focus on their ability to precisely estimate strata prevalence.
 
